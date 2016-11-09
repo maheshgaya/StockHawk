9b126be2e4f35759463f96bfd118ad4be3072d24
Mahesh Gaya <mmaheshgaya@gmail.com>
2016-11-09 10:28:51 -0600

    Feat: added LineChart for DetailActivity

--- app/src/main/java/com/sam_chordas/android/stockhawk/ui/DetailActivity.java
+++ app/src/main/java/com/sam_chordas/android/stockhawk/ui/DetailActivity.java
@@ -10,11 +10,20 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
 import com.sam_chordas.android.stockhawk.Constants;
 import com.sam_chordas.android.stockhawk.R;
 import com.sam_chordas.android.stockhawk.data.QuoteColumns;
 import com.sam_chordas.android.stockhawk.data.QuoteProvider;
 import com.sam_chordas.android.stockhawk.rest.Utils;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by Mahesh Gaya on 11/8/16.
@@ -23,7 +32,9 @@ import com.sam_chordas.android.stockhawk.rest.Utils;
 public class DetailActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor>{
     private String mSymbol;
     private static final int CURSOR_LOADER_ID = 1;
+    private static final String REGEX = "[^\\d.]+|\\.(?!\\d)";
     private Cursor mCursor;
+    private LineChart mChart;
     private static String TAG = DetailActivity.class.getSimpleName();
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -32,9 +43,11 @@ public class DetailActivity extends AppCompatActivity implements LoaderManager.L
         Uri uri = Uri.parse(getIntent().getStringExtra(Constants.EXTRA_DETAIL));
         //Log.d(TAG, "onCreate: " + uri);
         mSymbol = uri.getLastPathSegment(); //gets the symbol from the calling activity
-
         getLoaderManager().initLoader(CURSOR_LOADER_ID, null, this);
-        Utils.getCurrentDateTime();
+
+        mChart = (LineChart) findViewById(R.id.linechart);
+
+
     }
 
     @Override
@@ -61,6 +74,40 @@ public class DetailActivity extends AppCompatActivity implements LoaderManager.L
     @Override
     public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
         mCursor = data;
+        ArrayList<Float> values = new ArrayList<Float>(){};
+        ArrayList<Float> labels = new ArrayList<Float>(){};
+
+
+        if (mCursor.getCount() != 0){
+            while (mCursor.moveToNext()){
+                Float bidPrice = mCursor.getFloat(Constants.COLUMN_BIDPRICE);
+                values.add(bidPrice);
+                Pattern pattern = Pattern.compile(REGEX);
+                Matcher matcher = pattern.matcher(mCursor.getString(Constants.COLUMN_PERCENT_CHANGE));
+                //Log.d(TAG, "onLoadFinished: " + matcher.toString());
+                //labels.add(percentChange);
+            }
+        }
+
+        List<Entry> entries = new ArrayList<Entry>();
+        for (int i = 0; i < values.size(); i++) {
+
+            // turn your data into Entry objects
+            //labels.get(i)
+            entries.add(new Entry(i, values.get(i)));
+        }
+
+        LineDataSet dataSet = new LineDataSet(entries, "Label"); // add entries to dataset
+        dataSet.setColor(getResources().getColor(R.color.material_blue_700));
+        dataSet.setValueTextColor(getResources().getColor(android.R.color.black)); // styling, ...
+        LineData lineData = new LineData(dataSet);
+        mChart.setData(lineData);
+
+        mChart.invalidate(); // refresh
+
+
+
+
     }
 
     @Override
