2c71c8fe1ca01c8aa8bd4723d8087f1419b4eae7
Mahesh Gaya <mmaheshgaya@gmail.com>
2016-11-14 22:42:36 -0600

    WIP: widget Provider

---
+++ app/src/main/java/com/sam_chordas/android/stockhawk/ui/StockAppWidgetProvider.java
@@ -0,0 +1,16 @@
+package com.sam_chordas.android.stockhawk.ui;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+
+/**
+ * Created by Mahesh Gaya on 11/14/16.
+ */
+
+public class StockAppWidgetProvider extends AppWidgetProvider{
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+    }
+}
